0f8eea5a407adae735ad2455b39b9113d7319231
Buddy Delaune <BCDel89@gmail.com>
2024-08-11 12:37:59 -0500

    init commit 2

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,20 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Nion Lights LLC",
+    "email": "nionlightsllc@gmail.com",
+    "phoneForTel": null,
+    "phoneFormatted": null,
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "lineOne": null,
+        "lineTwo": null,
+        "city": "New Orleans",
+        "state": "LA",
+        "zip": null,
+        "mapLink": null
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.nionlights.com"
 };
